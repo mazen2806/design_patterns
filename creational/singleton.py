5b797e9b298ae6e4b2cece583e1fee89a53efcb2
Yuliia T <tomazenko@gmail.com>
2023-03-30 18:25:00 +0300

    Added decorator for singleton pattern

--- creational/singleton.py
+++ creational/singleton.py
@@ -1,10 +1,26 @@
 
 class Singleton:
     """
-    Singleton is a design pattern that allows  to create just one instance of a class
+    Singleton is a design pattern that ensures that a class can only have one instance, and provides a global point of access to that instance
     """
 
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(Singleton, cls).__new__(cls)
         return cls.instance
+
+
+# singleton can be implemented like a decorator
+def singleton_decorator(cls):
+    instances = {}
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return get_instance
+
+
+@singleton_decorator
+class C:
+    def __init__(self, name):
+        self.name = name
--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -1,5 +1,6 @@
 import unittest
-from creational.singleton import Singleton
+from creational.singleton import Singleton, C
+
 
 
 class TestSingletonClass(unittest.TestCase):
@@ -8,3 +9,10 @@ class TestSingletonClass(unittest.TestCase):
         s2 = Singleton()
 
         self.assertIs(s1, s2)
+
+    def test_singleton_decorator(self):
+        c1 = C(1)
+        c2 = C(2)
+
+        self.assertIs(c1, c2)
+        self.assertEqual(c1.name, c2.name)
